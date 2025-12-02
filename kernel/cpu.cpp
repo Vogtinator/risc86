788c5d0cdfc7b0b030cb204bec2329d07a999b3e
Fabian Vogt <fvogt@suse.de>
2025-12-02 13:33:51 +0100

    Some instructions

--- kernel/cpu.cpp
+++ kernel/cpu.cpp
@@ -31,6 +31,44 @@ static void dumpCPUState(HartState *hart)
 	}
 }
 
+static inline uint64_t getReg(HartState *hart, int r)
+{
+	return hart->regs[r];
+}
+
+static inline void setReg(HartState *hart, int r, uint64_t value)
+{
+	if (r != 0)
+		hart->regs[r] = value;
+}
+
+static uint64_t getCSR(HartState *hart, uint16_t csr)
+{
+	switch (csr) {
+	default:
+		panic("Unknown CSR 0x%03x", csr);
+	}
+
+	return 0;
+}
+
+static void setCSR(HartState *hart, uint16_t csr, uint64_t value)
+{
+	switch (csr) {
+	case 0x104u:
+		hart->sie = value;
+		return;
+	case 0x106u:
+		hart->scounteren = value & 3;
+		return;
+	case 0x144u:
+		hart->sip = value;
+		return;
+	default:
+		panic("Unknown CSR 0x%03x", csr);
+	}
+}
+
 void runThisCPU()
 {
 	HartState *hart = &getPerCPU()->hart;
@@ -48,18 +86,209 @@ void runThisCPU()
 		// 16-bit wide compressed instruction?
 		if ((inst16 & 0b11) != 0b11)
 		{
-			printf("Instruction: %04x\n", inst16);
+			const uint16_t inst = inst16;
+			printf("Instruction: %04x\n", inst);
+
+			if ((inst & 0xE003) == 0xA001) { // c.j
+				// This is atrocious.
+				uint16_t imm11 = (inst >> 12) & 1,
+						 imm4  = (inst >> 11) & 1,
+						 imm98 = (inst >>  9) & 3,
+						 imm10 = (inst >>  8) & 1,
+						 imm6  = (inst >>  7) & 1,
+						 imm7  = (inst >>  6) & 1,
+						 imm31 = (inst >>  3) & 7,
+						 imm5  = (inst >>  2) & 1;
+
+				uint16_t imm = (imm11 << 11) | (imm10 << 10) | (imm98 << 8)
+						| (imm7 << 7) | (imm6 << 6) | (imm5 << 5)
+						| (imm4 << 4) | (imm31 << 1);
+
+				const int16_t offs = int16_t(imm << 4) >> 4;
+
+				hart->pc += offs;
+				continue;
+			} else if ((inst & 0xE003) == 0x4001) { // c.li
+				uint16_t imm40 = (inst >> 2) & 0x1F,
+						 imm5  = (inst >> 12) & 1;
+
+				int16_t imm = int16_t(((imm5 << 5) | imm40) << 10) >> 10;
+				uint32_t rd = (inst >> 7) & 0x1F;
+				setReg(hart, rd, uint64_t(imm));
+			} else if ((inst & 0xEF83) == 0x6101) { // c.addi16sp
+				uint16_t imm9  = (inst >> 12) & 1,
+						 imm4  = (inst >>  6) & 1,
+						 imm6  = (inst >>  5) & 1,
+						 imm87 = (inst >>  3) & 3,
+						 imm5  = (inst >>  2) & 1;
+
+				uint16_t uimm = (imm9 << 9) | (imm87 << 7) | (imm6 << 6)
+						| (imm5 << 5) | (imm4 << 4);
+
+				int16_t imm = int16_t(uimm << 5) >> 5;
+				uint32_t rd = 2;
+				setReg(hart, rd, getReg(hart, 2) + imm);
+			} else if ((inst & 0b111'0'00000'00000'11) == 0b011'0'00000'00000'01) { // c.lui
+				uint16_t imm1612 = (inst >> 2) & 0x1F,
+						 imm17   = (inst >> 12) & 1;
+
+				int32_t imm = int32_t(((imm17 << 17) | (imm1612 << 12)) << 14) >> 14;
+				uint32_t rd = (inst >> 7) & 0x1F;
+				setReg(hart, rd, uint64_t(imm));
+			} else {
+				panic("Unknown instruction");
+			}
+
 			hart->pc += 2;
 			continue;
 		}
 
 		// (at least) 32-bit wide instruction. Fetch the remaining 16 bits.
-		uint32_t inst32 = inst16;
+		uint32_t inst = inst16;
 		if (!fetchInstruction(hart, &inst16, hart->pc + 2))
 			panic("Instruction fault");
 
-		inst32 |= inst16 << 16;
-		printf("Instruction: %04x\n", inst32);
+		inst |= inst16 << 16;
+		printf("Instruction: %04x\n", inst);
+
+		uint32_t opc = inst & 0x7Fu;
+		switch(opc) {
+		case 0x13u: // integer immediate
+		{
+			uint32_t funct3 = (inst >> 12u) & 7u;
+			uint32_t rd = (inst >> 7u) & 31u;
+			uint32_t rs1 = (inst >> 15u) & 31u;
+			int64_t imm = int32_t(inst) >> 20u;
+			uint64_t rawimm = inst >> 20u;
+			switch (funct3)
+			{
+				case 0x0u: // addi
+					setReg(hart, rd, int64_t(getReg(hart, rs1)) + imm);
+					break;
+				case 0x1u: // slli
+					if ((rawimm >> 6u) == 0) // slli
+						setReg(hart, rd, getReg(hart, rs1) << (rawimm & 63u));
+					else
+						panic("Shift not supported");
+
+					break;
+				case 0x2u: // slti
+					setReg(hart, rd, (int64_t(getReg(hart, rs1)) < imm) ? 1u : 0u);
+					break;
+				case 0x3u: // sltiu
+					setReg(hart, rd, (getReg(hart, rs1) < rawimm) ? 1u : 0u);
+					break;
+				case 0x4u: // xori
+					setReg(hart, rd, getReg(hart, rs1) ^ rawimm);
+					break;
+				case 0x5u: // sr(l,a)i
+					if ((rawimm >> 6u) == 0) // srli
+						setReg(hart, rd, getReg(hart, rs1) >> (rawimm & 63u));
+					else if ((rawimm >> 6u) == 0x10u) // srai
+						setReg(hart, rd, int64_t(getReg(hart, rs1)) >> (rawimm & 63u));
+					else
+						panic("Shift not supported");
+
+					break;
+				case 0x6u: // ori
+					setReg(hart, rd, getReg(hart, rs1) | rawimm);
+					break;
+				case 0x7u: // andi
+					setReg(hart, rd, getReg(hart, rs1) & rawimm);
+					break;
+				default:
+					panic("Unsupported instruction");
+			}
+			break;
+		}
+		case 0x17u: // auipc
+		{
+			uint32_t rd = (inst >> 7u) & 0x1Fu;
+			setReg(hart, rd, hart->pc + (inst & 0xFFFFF000u));
+			break;
+		}
+		case 0x73u: // SYSTEM
+		{
+			uint32_t funct3 = (inst >> 12u) & 0x7u;
+			switch(funct3)
+			{
+			case 1u: // CSRRW
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				unsigned int rs1 = (inst >> 15u) & 31u;
+				uint64_t rs1val = getReg(hart, rs1);
+				if (rd != 0u) { // No getCSR side effect if rd is zero
+					setReg(hart, rd, getCSR(hart, csr));
+				}
+				setCSR(hart, csr, rs1val);
+				break;
+			}
+			case 2u: // CSRRS
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				unsigned int rs1 = (inst >> 15u) & 31u;
+				uint64_t csrval = getCSR(hart, csr);
+				uint64_t rs1val = getReg(hart, rs1);
+				setReg(hart, rd, getCSR(hart, csr));
+				setCSR(hart, csr, csrval | rs1val);
+				break;
+			}
+			case 3u: // CSRRC
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				unsigned int rs1 = (inst >> 15u) & 31u;
+				uint64_t csrval = getCSR(hart, csr);
+				uint64_t rs1val = getReg(hart, rs1);
+				setReg(hart, rd, getCSR(hart, csr));
+				setCSR(hart, csr, csrval & ~rs1val);
+				break;
+			}
+			case 5u: // CSRRWI
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				if (rd != 0u) { // No getCSR side effect if rd is zero
+					setReg(hart, rd, getCSR(hart, csr));
+				}
+				uint64_t imm = (inst >> 15u) & 31u;
+				setCSR(hart, csr, imm);
+				break;
+			}
+			case 6u: // CSRRSI
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				uint64_t imm = (inst >> 15u) & 31u;
+				uint64_t csrval = getCSR(hart, csr);
+				setReg(hart, rd, csrval);
+				setCSR(hart, csr, csrval | imm);
+				break;
+			}
+			case 7u: // CSRRCI
+			{
+				uint16_t csr = inst >> 20u;
+				unsigned int rd = (inst >> 7u) & 31u;
+				uint64_t imm = (inst >> 15u) & 31u;
+				uint64_t csrval = getCSR(hart, csr);
+				setReg(hart, rd, csrval);
+				setCSR(hart, csr, csrval & ~imm);
+				break;
+			}
+			default:
+				panic("Unknown SYSTEM instruction");
+			}
+
+			// Immediately check for interrupts
+			hart->pc += 4;
+			continue;
+		}
+		default:
+			panic("Unknown instruction");
+		}
+
 		hart->pc += 4;
 	}
 }
--- kernel/hart.h
+++ kernel/hart.h
@@ -7,5 +7,7 @@ struct HartState {
 	uint64_t regs[32];
 	uint64_t pc;
 	// CSRs
+	uint64_t sie, sip;
 	uint64_t satp;
+	uint32_t scounteren; // Only & 3 supported
 };
