188a6d5657b65710949527d034f405221838bfa2
Fabian Vogt <fabian@ritter-vogt.de>
2026-01-16 17:48:49 +0100

    JIT translate 32-bit integer register instructions

--- kernel/x86jit.h
+++ kernel/x86jit.h
@@ -76,6 +76,7 @@ private:
 	X86Reg mapRVRegForRead(RVReg rvReg, bool bits32Ok);
 	X86Reg mapRVRegForRead64(RVReg rvReg);
 	X86Reg mapRVRegForRead32(RVReg rvReg);
+	X86Reg mapRVRegForWrite(RVReg rvReg, bool is32bits);
 	X86Reg mapRVRegForWrite32(RVReg rvReg);
 	X86Reg mapRVRegForWrite64(RVReg rvReg);
 	X86Reg mapRVRegForReadWrite64(RVReg rvReg);
