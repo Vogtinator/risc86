5b5e5eb8988c0a37802feb7f9f52876f2f10df6a
Fabian Vogt <fabian@ritter-vogt.de>
2026-01-25 18:10:55 +0100

    Introduce X86JIT::emitMovMem helper
    One place to satisfy most ModR/M needs.

--- kernel/x86jit.h
+++ kernel/x86jit.h
@@ -53,6 +53,7 @@ private:
 	void emitMovRegReg(X86Reg from, X86Reg to);
 	void emitXorRegReg(X86Reg x86Reg);
 	void emitCliHlt(); // For debugging
+	void emitMovMem(X86Reg base, int32_t disp, X86Reg data, bool isLoad, uint8_t size);
 
 	// Low-level helpers for RV register management
 	static const X86Reg hartPtrReg = X86Reg::RDI, hartPCReg = X86Reg::R12;
